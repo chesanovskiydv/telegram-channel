587dca2d131a9ada1d415578ef4460678b301696
chesanovskiy_dv <chesanovskiydv@gmail.com>
2019-07-21 12:40:39 +0300

    add message validation for unique data

--- src/scrapers/pipelines.py
+++ src/scrapers/pipelines.py
@@ -5,9 +5,11 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 from scrapy import Item
+from scrapers.items import MessageItem
+
+from sqlalchemy.orm import aliased
 
 from database import session
-from scrapers.items import MessageItem
 from database.models import Message
 
 
@@ -20,6 +22,23 @@ class MessagePipeline(object):
     @staticmethod
     def save_item(item: Item, spider):
         if isinstance(item, MessageItem):
-            with session.begin():
-                message = Message(text=item.get('text'), image=item.get('image'), url=item.get('url'))
-                session.add(message)
+
+            def message_is_unique(message_model, limit=20):
+                subquery = session.query(Message) \
+                    .order_by(Message.created_at.desc(), Message.id.desc()) \
+                    .limit(limit) \
+                    .subquery()
+
+                alias = aliased(Message, subquery)
+
+                return not session.query(
+                    session.query(alias).filter(
+                        alias.text == message_model.text, alias.image == message_model.image
+                    ).exists()
+                ).scalar()
+
+            message = Message(text=item.get('text'), image=item.get('image'), url=item.get('url'))
+
+            if message_is_unique(message):
+                with session.begin():
+                    session.add(message)
